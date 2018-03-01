2c82aa5436012fb620c6cfed72b5cb2b9c718515
Mia <miamollie@gmail.com>
2018-03-01 22:01:08 +1100

    Update gallery

--- assets/main.js
+++ assets/main.js
@@ -1,22 +1,57 @@
   $(document).ready(function(){
 
-    //Menu toggle
-    $('nav button').click(function(){
+    // Menu toggle
+    $('nav button').click( function() {
       $('body').toggleClass("no-scroll");
       $('nav').toggleClass("menu-open");
       $('nav ul').toggleClass("hide-sm");
-      if ( $('nav h3').text() == 'menu') {
-        $('nav h3').text('X');
-      } else {
-        $('nav h3').text('menu');
-      }
+      $('nav i').toggleClass("fa-bars");
+      $('nav i').toggleClass  ("fa-window-close");
     });
 
-    //MENU SELECTED
+    // Menu
     $('nav li').click(function() {
       $('body').removeClass('no-scroll');
       $('nav').removeClass("menu-open");
       $('nav ul').addClass("hide-sm");
       $('nav h3').text('menu');
     });
-  })
+
+    // Gallery tabs
+    $('[data-tab]').on('click', toggleGalleryTab);
+
+    $('#dropdown-trigger').on('click', toggleDropdown);
+
+  });
+
+  $(window).load(function() {
+    updateMasonry();
+  });
+
+  var toggleDropdown =  function() {
+    $('#dropdown-body').toggleClass('show');
+    $('#dropdown-trigger i').toggleClass('fa-angle-up');
+    $('#dropdown-trigger i').toggleClass('fa-angle-down');
+  }
+
+  var toggleGalleryTab = function() {
+    $('.tabs h2').removeClass('active');
+    $(this).addClass('active');
+
+    $('.tab-content').removeClass('show');
+
+    var currentContent = $(this).attr('data-tab');
+    $('#' + currentContent).addClass('show');
+
+    setTimeout( function() {
+      updateMasonry();
+    }, 500);
+  }
+
+  var updateMasonry = function() {
+    $('.masonry').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: '.grid-sizer',
+        gutter: 10,
+    })
+  }
